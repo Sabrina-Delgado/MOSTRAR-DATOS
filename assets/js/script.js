42a141e730dfe12ed8d6358b279e9b87d769d1a8
Sabrina-Delgado <sdelgado@estudiantes.crackthecode.la>
2021-12-23 14:20:45 -0500

    penultimo commit

---
+++ assets/js/script.js
@@ -0,0 +1,295 @@
+//--------------------------------------------------------------------------------------
+// boton activadot = id="escoge-carrera"
+// div a aparecer = id="menu-busca-carrera"
+// boton para cerrar = id="menu1Cerrar"
+
+$(document).ready(function () {
+  document.getElementById("menu-busca-carrera").style.display= "none"
+  $("#escoge-carrera").click(function () {
+    $("#menu-busca-carrera").show("slow");//mostrar
+  });
+  $("#menu1Cerrar").click(function () {
+    $("#menu-busca-carrera").hide("slow"); //ocultar
+  });
+  
+});
+
+
+//--------------------------------------------------------------------------------------
+// botom activador =  id="registrate-li"
+// div aparecer = id="form-cerrar"
+// boton cerrar = id="formulario-registrate"
+$(document).ready( function () {
+    document.getElementById("formulario-registrate").style.display="none";
+    $("#registrate-li").click(function (){
+        $("#formulario-registrate").show("slow");
+    })
+    $("#form-cerrar").click(function (){
+        $("#formulario-registrate").hide("slow");
+    }) 
+})
+//--------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+//Function Igieneria de alimentos
+// botom activador =  id= image-carrera-1
+// div a aparecer = id= aparecer-carreras
+$(document).ready(function () {
+    document.getElementById("ingalimentos").style.display= "none"
+    $("#imageingalimentos").click(function () {
+      $("#ingalimentos").show("slow");//mostrar
+    });
+    $("#cerrarcarreraingalimentoss").click(function () {
+      $("#ingalimentos").hide("slow"); //ocultar
+    });
+    
+});
+//--------------------------------------------------------------------------------------------------------------------
+//Function Traductores
+// botom activador =  id= imagetraductores
+// div a aparecer = id= traductores
+// butom cerrar = id=cerrarcarreratraductores
+$(document).ready(function () {
+    document.getElementById("traductores").style.display= "none"
+    $("#imagetraductores").click(function () {
+      $("#traductores").show("slow");//mostrar
+    });
+    $("#cerrarcarreratraductores").click(function () {
+      $("#traductores").hide("slow"); //ocultar
+    });
+    
+});
+
+//--------------------------------------------------------------------------------------------------------------------
+//Function Nutricion
+// botom activador =  id= imagenutricion
+// div a aparecer = id= nutricion
+// butom cerrar = id=cerrarcarreranutricion
+$(document).ready(function () {
+    document.getElementById("nutricion").style.display= "none"
+    $("#imagenutricion").click(function () {
+      $("#nutricion").show("slow");//mostrar
+    });
+    $("#cerrarcarreranutricion").click(function () {
+      $("#nutricion").hide("slow"); //ocultar
+    });
+    
+});
+//--------------------------------------------------------------------------------------------------------------------
+//Function Cine y Audiovisuales
+// botom activador =  id="imagecine"
+// div a aparecer = id="cine"
+// butom cerrar = id="cerrarcarreracine"
+$(document).ready(function () {
+    document.getElementById("cine").style.display= "none"
+    $("#imagecine").click(function () {
+      $("#cine").show("slow");//mostrar
+    });
+    $("#cerrarcarreracine").click(function () {
+      $("#cine").hide("slow"); //ocultar
+    });
+    
+});
+//--------------------------------------------------------------------------------------------------------------------
+//Function Agronomia
+// botom activador =  id="imageagronomia"
+// div a aparecer = id="agronomia"
+// butom cerrar = id="cerrarcarreraagronomia"
+$(document).ready(function () {
+    document.getElementById("agronomia").style.display= "none"
+    $("#imageagronomia").click(function () {
+      $("#agronomia").show("slow");//mostrar
+    });
+    $("#cerrarcarreraagronomia").click(function () {
+      $("#agronomia").hide("slow"); //ocultar
+    });
+    
+});
+//--------------------------------------------------------------------------------------------------------------------
+//Function Periodismo
+// botom activador =  id="imageperiodismo"
+// div a aparecer = id="periodismo"
+// butom cerrar = id="cerrarcarreraperiodismo"
+$(document).ready(function () {
+    document.getElementById("periodismo").style.display= "none"
+    $("#imageperiodismo").click(function () {
+      $("#periodismo").show("slow");//mostrar
+    });
+    $("#cerrarcarreraperiodismo").click(function () {
+      $("#periodismo").hide("slow"); //ocultar
+    });
+    
+});
+//--------------------------------------------------------------------------------------------------------------------
+//Function Ver mas 2
+// botom activador =  id="mcarreras1"
+// div a aparecer = id="vermas2"
+// div a desaparecer = id="vermas1"
+$(document).ready(function () {
+    document.getElementById("vermas2").style.display= "none"
+    $("#mcarreras1").click(function () {
+      $("#vermas2").show("slow");//mostrar
+    });
+    $("#mcarreras1").click(function () {
+      $("#vermas1").hide("slow"); //ocultar
+    });
+    
+});
+/*
+$(document).ready(function () {
+    document.getElementById("vermas2").style.display= "none"
+    $("#+carreras1").click(function () {
+      $("#vermas1").hide("slow"); //ocultar  
+      $("#vermas2").show("slow");//mostrar
+    });        
+});
+*/
+
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+//Function Diseño Grafico
+// botom activador =  id="imagedgrafico"
+// div a aparecer = id="dgrafico"
+// butom cerrar = id="cerrarcarreradgrafico"
+$(document).ready(function () {
+    document.getElementById("dgrafico").style.display= "none"
+    $("#imagedgrafico").click(function () {
+      $("#dgrafico").show("slow");//mostrar
+    });
+    $("#cerrarcarreradgrafico").click(function () {
+      $("#dgrafico").hide("slow"); //ocultar
+    });
+    
+});
+//--------------------------------------------------------------------------------------------------------------------
+//Function Bioquimica
+// botom activador =  id="imagebioquimica"
+// div a aparecer = id="bioquímica"
+// butom cerrar =  id="cerrarcarrerabioquímica"
+$(document).ready(function () {
+    document.getElementById("bioquímica").style.display= "none"
+    $("#imagebioquimica").click(function () {
+      $("#bioquímica").show("slow");//mostrar
+    });
+    $("#cerrarcarrerabioquímica").click(function () {
+      $("#bioquímica").hide("slow"); //ocultar
+    });
+});
+//--------------------------------------------------------------------------------------------------------------------
+//Function Fotografia
+// botom activador =  id="imagefoto"
+// div a aparecer = id="foto"
+// butom cerrar =  id="cerrarcarrerafoto"
+$(document).ready(function () {
+    document.getElementById("foto").style.display= "none"
+    $("#imagefoto").click(function () {
+      $("#foto").show("slow");//mostrar
+    });
+    $("#cerrarcarrerafoto").click(function () {
+      $("#foto").hide("slow"); //ocultar
+    });
+});
+//--------------------------------------------------------------------------------------------------------------------
+//Function Bromatilogia
+// botom activador =  id="imagebromatologia"
+// div a aparecer = id="bromatología"
+// butom cerrar =  id="cerrarcarrerabromatologia"
+$(document).ready(function () {
+    document.getElementById("bromatología").style.display= "none"
+    $("#imagebromatologia").click(function () {
+      $("#bromatología").show("slow");//mostrar
+    });
+    $("#cerrarcarrerabromatologia").click(function () {
+      $("#bromatología").hide("slow"); //ocultar
+    });
+});
+//--------------------------------------------------------------------------------------------------------------------
+//Function Veterinaria
+// botom activador =  id="imageveterinaria"
+// div a aparecer = id="veterinaria"
+// butom cerrar =  id="cerrarcarreraveterinaria"
+$(document).ready(function () {
+    document.getElementById("veterinaria").style.display= "none"
+    $("#imageveterinaria").click(function () {
+      $("#veterinaria").show("slow");//mostrar
+    });
+    $("#cerrarcarreraveterinaria").click(function () {
+      $("#veterinaria").hide("slow"); //ocultar
+    });
+});
+//--------------------------------------------------------------------------------------------------------------------
+//Function Diseño de Indumentaria
+// botom activador =  id="imagemoda"
+// div a aparecer = id="moda"
+// butom cerrar =  id="cerrarcarreramoda"
+$(document).ready(function () {
+    document.getElementById("bromatología").style.display= "none"
+    $("#imagebromatologia").click(function () {
+      $("#bromatología").show("slow");//mostrar
+    });
+    $("#cerrarcarrerabromatologia").click(function () {
+      $("#bromatología").hide("slow"); //ocultar
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+var carrerras = {
+  comunicaciones: {
+    traductores : $("#litraductores"),//comu
+    cine : $("#licine"), //comu
+    foto : $("#lifoto"),//comu
+    moda : $("#limoda")  //comu
+  },
+  medicina: {
+    bioquimica: $("#libioquimica"),//ing
+    bromatologia:$("#libromatologia"),//ing
+    veterinaria: $("#liveterinaria"), //ing
+    bioquimica: $("#libioquimica"),//ing
+    alimentos: $("#liingalimentos"),//ing
+    nutricion: $("#linutricion"),//ing
+    agronomia: $("#liagronomia"),//ing
+  }
+}
+
+
+//-------------------------------------------------------------------------------
+//Comunicaciones
+// botom activador =  id="comunicacionescomun"
+// div a aparecer = comunicaciones
+// butom cerrar =  
+
+$(document).ready(function () {
+  
+  $("#comunicacionescomun").click(function () {
+    comunicaciones.hide("slow"); //ocultar
+  });
+  $("#comunicacionescomun").click(function () {
+    carrerras[1].show("slow");//mostrar
+  });
+});
+
+//-------------------------------------------------------------------------------
+//Medicina
+// botom activador =  id="medicinacomun"
+// div a aparecer = medicina
+// butom cerrar =  
---
+++ readme.md
@@ -0,0 +1,29 @@
+EXPERIENCIA DE CREADOR
+--Preguntas de interaccion--
+¿Quiénes son los principales usuarios del producto.?
+Personas que necesitan asesoria para descubrir a que se vana a dedicar, segun rasgos de distintas carreras que les guste mas; por eso especificamos de que se trata cada una.
+
+¿Cuáles son los objetivos de estos usuarios en relación con tu producto.?
+Descubrir cual es la carrera que quieren llegar aestudiar.
+
+¿Cómo crees que el producto que estás creando está resolviendo sus problemas?
+Los resulve dandoles mas opciones de elegir, proporcionando informacion de carreras que capaz no conocian, y de su utilidad, ademas de una explicacion sencilla y sercana, sin dejar de ser respetuosa
+
+
+--Comentarios--
+-Al inicio tuvimos prolemas con Java Script, no sabiamos como concretamente crear un "objeto", y que relacion tendria con el array del que tanto se nos hablaba, sin embargo con lepaso del tiempo pudimos comprender que era darle valor a cada variable y determinar su interior con arrays.
+
+-Se nos hizo complicado el echo de que sea una interfaz visualmente adaptable, fue en lo que ams tiempo pusimos en el proyecto, ya que habian cosas que por nuestra propia cuenta investigamos, cierta parte del proyecto completo este crierio, pero no del todo.
+
+-Las creaciones de nuestros portotipos, solo lo hicimos en una hoja, no pudimos uzar Figma u otra aplicacion, debido al tiempo que tuvimos, y al no saber el manejo, intentamos con otras aplicaciones,pero lo primoldiar del proyecto aun no se concretaba y era de necesidad terminarlo.
+
+
+-Si bien es cierto, no tenemos un archivo .json, en nuestro Java creamos objetos , que hubieramos creado en un archivo Json, incluso intentamos realizar este archivo, sim embargo, se nos hizo mas facil realizar los objetos en JavaScript
+
+-Queremos que esta pagina, cumpla con las necesidades de una interfaz interactiva, y de buen contenido, estamos en la ultima semana y aunque tengamos la informacion y gran parte de la presentacion, CSS no lo hemos tocado aun por la prioridad de Java, y aun nos falta una pequeña parte en este archivo.
+
+-Nos basamos en otras paginas y en una sobretodo, link de referencia:https://www.nosequeestudiar.net/carreras/
+
+--Creadoras--
+Sabrina Delgado
+Estefany Chavez
